2e979580fc135e8ef53785f9228f81eeeba7d736
Moustachaus <moustachaus@gmail.com>
2019-02-03 09:00:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ bot.on('message', message => {
     if (message.author.bot) return;
 
 
-    if(splitMessage[0] === '!reglement') {
-        message.reply("test")
-        }
+    if (message.content.startsWith("!reglement")) {
+        var reglement = new Discord.RichEmbed()
+        .setTitle("Règlements !")
+        .setDescription("Voici les règlements" + member.user.username)
+        .addField("**abc: **", "abc", false)
+        .setColor("0xE61919")
+        .setFooter("Kiwi")
+    
+  }
 
 
 
