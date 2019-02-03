933b9cf67ec4b8d314705b6412821c245fb3204d
Moustachaus <moustachaus@gmail.com>
2019-02-03 09:13:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ bot.on('message', message => {
     if (message.content.startsWith("!reglement")) {
         var reglement = new Discord.RichEmbed()
             .setTitle("Règlements !")
-            .setDescription("Voici les règlements" + member.user.username)
-            .addField("**abc: **", "abc", false)
+            .setDescription("Voici les règlements")
+            .addField("**abc:", "abc", true)
             .setColor("0xE61919")
             .setFooter("Kiwi")
     
